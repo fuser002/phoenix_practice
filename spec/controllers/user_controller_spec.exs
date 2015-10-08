e7bb6a277a63f90e86bc39d2382d1857eeea35ce
Yu Hoshino <yhoshino11@gmail.com>
2015-10-08 21:43:15 +0900

    Finish all CRUD specs for User

--- spec/controllers/user_controller_spec.exs
+++ spec/controllers/user_controller_spec.exs
@@ -32,6 +32,31 @@ defmodule PhoenixPractice.UserControllerSpec do
     it "create" do
       res_conn = action(:create, %{"user" => %{name: "yhoshino11"}})
       expect(res_conn).to redirect_to(user_path(conn, :index))
+      expect(Repo.all(User) |> Enum.count).to eq(1)
+    end
+
+    it "create - invalid" do
+      res_conn = action(:create, %{"user" => %{}})
+      expect(res_conn).to be_successful
+      expect(res_conn).to render_template("new.html")
+      expect(Repo.all(User) |> Enum.count).to eq(0)
+    end
+
+    it "edit" do
+      Repo.insert(%User{id: 1, name: "yhoshino11"})
+      user = Repo.get!(User, 1)
+      res_conn = action(:edit, %{"id" => 1})
+      expect(res_conn).to be_successful
+      expect(res_conn).to render_template("edit.html")
+      expect(res_conn).to have_in_assigns(user: user, changeset: User.changeset(user))
+    end
+
+    it "update" do
+      Repo.insert(%User{id: 1, name: "yhoshino11"})
+      user = Repo.get!(User, 1)
+      res_conn = action(:update, %{"id" => 1, "user" => %{name: "yhoshino12"}})
+      expect(res_conn).to redirect_to(user_path(conn, :show, user))
+      expect(Repo.get!(User, 1) |> Map.fetch(:name)).to eq({:ok, "yhoshino12"})
     end
   end
 end
